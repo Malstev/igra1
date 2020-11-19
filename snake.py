a4f312cd6a7a6b57e8ef381d9827c89ce1fd94ba
Malstev <maltsev.evgenii.env@yandex.com>
2020-11-19 16:22:22 +0800

    Now snake can eat itself and finished the game

--- snake.py
+++ snake.py
@@ -53,12 +53,22 @@ class Snake:
         '''
         will must recognize the touch of the apple by the snake
         '''
-        global speed_multiplier, body
+        global speed_multiplier, body, i, snake_speed
         if self.pos_x == apple.pos_x and self.pos_y == apple.pos_y:
             speed_multiplier += 1
             canvas.delete(apple.shape)
-            body.append(Body(snake.pos_x, snake.pos_y))
+            if snake.vx == 40:
+                body.append(Body(body[speed_multiplier-2].pos_x - snake.size, body[speed_multiplier-2].pos_y))
+            if snake.vx == -40:
+                body.append(Body(body[speed_multiplier-2].pos_x + snake.size, body[speed_multiplier-2].pos_y))
+            if snake.vy == 40:
+                body.append(Body(body[speed_multiplier-2].pos_x, body[speed_multiplier-2].pos_y - snake.size))
+            if snake.vy == -40:
+                body.append(Body(body[speed_multiplier - 2].pos_x, body[speed_multiplier - 2].pos_y + snake.size))
             apple.__init__()
+        for i in body:
+            if i.pos_x == self.pos_x and i.pos_y == self.pos_y:
+                game_over()
 
     def up(self, event=None):
         if -40 < self.vy < 40:
@@ -97,8 +107,8 @@ class Apple:
     def __init__(self):
         '''initialize the apples - the main target in the game'''
         self.size = 40
-        self.pos_x = choice(positions)
-        self.pos_y = choice(positions)
+        self.pos_x = choice(x_positions)
+        self.pos_y = choice(y_positions)
         self.shape = canvas.create_rectangle(self.pos_x,
                                              self.pos_y,
                                              self.pos_x + self.size,
@@ -107,33 +117,41 @@ class Apple:
 
 
 def refresh():
-    for i in range(len(body)-1, -1, -1):
-        if not i:
-            body[i].pos_x = snake.pos_x
-            body[i].pos_y = snake.pos_y
-        else:
-            body[i].pos_x = body[i - 1].pos_x
-            body[i].pos_y = body[i - 1].pos_y
-    root.after(snake_speed, snake.move)
-    root.after(snake_speed, snake.check_collision)
-    if body:
-        for i in body:
-            root.after(snake_speed, i.set_body_coords)
+    if snake.vx != 0 or snake.vy != 0:
+        for i in range(len(body)-1, -1, -1):
+            if not i:
+                body[i].pos_x = snake.pos_x
+                body[i].pos_y = snake.pos_y
+            else:
+                body[i].pos_x = body[i - 1].pos_x
+                body[i].pos_y = body[i - 1].pos_y
+        root.after(snake_speed, snake.move)
+        root.after(snake_speed, snake.check_collision)
+        if body:
+            for i in body:
+                root.after(snake_speed, i.set_body_coords)
     root.after(snake_speed, refresh)
 
 
+def game_over():
+    canvas.delete(tk.ALL)
+    canvas.create_text(240, 240, text='GAME OVER', font=72, anchor='center')
+    root.after(2000, root.destroy)
+
+
 def main():
-    global canvas, root, snake, positions, apple, speed_multiplier, snake_speed, body
+    global canvas, root, snake, apple, speed_multiplier, snake_speed, body, x_positions, y_positions
     root = tk.Tk()
     speed_multiplier = 1
-    snake_speed = 500
-    positions = [40 * i for i in range(1, 12)]
-    pos_x, pos_y = choice(positions), choice(positions)
+    snake_speed = 500 - round(5*0.5**(-speed_multiplier))  # doesn`t work, need to put in another place and choose coorect function
+    x_positions = [40 * i for i in range(1, 12)]
+    y_positions = [40 * i for i in range(1, 12)]
+    pos_x, pos_y = choice(x_positions), choice(y_positions)
     canvas = tk.Canvas(root, width=480, height=480)
     canvas.pack()
-    apple = Apple()
-    body = []
     snake = Snake(pos_x, pos_y, 0, 0)
+    body = [Body(snake.pos_x - snake.size, snake.pos_y)]
+    apple = Apple()
     root.bind('<Down>', snake.down)
     root.bind('<Up>', snake.up)
     root.bind('<Left>', snake.left)
